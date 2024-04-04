23726c0c1e737f0c6ab94c7ccb656f2735565dbc
fujivara <marat.skyba@gmail.com>
2024-04-04 19:02:35 +0300

    Implemented compute handler

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,24 +4,51 @@ import (
 	"flag"
 	"fmt"
 	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"io"
+	"os"
+	"strings"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputName       = flag.String("f", "", "Input file name")
+	outputName      = flag.String("o", "", "Output file name")
 )
 
 func main() {
 	flag.Parse()
+	var readStream io.Reader
+	var writeStream io.Writer
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if len(*inputExpression) < 1 && len(*inputName) < 1 {
+		fmt.Fprintln(os.Stderr, "Input files must be provided")
+		os.Exit(1)
+	}
 
-	res, _ := lab2.PrefixToInfix("* - 4 2 6")
-	fmt.Println(res)
+	if len(*inputExpression) > 0 {
+		readStream = strings.NewReader(*inputExpression)
+	} else if file, err := os.Open(*inputName); err == nil {
+		defer file.Close()
+		readStream = file
+	} else {
+		fmt.Fprintln(os.Stderr, "Input file does not exist")
+		os.Exit(1)
+	}
+
+	if len(*outputName) < 1 {
+		writeStream = os.Stdout
+	} else if file, err := os.Create(*outputName); err == nil {
+		defer file.Close()
+		writeStream = file
+	} else {
+		fmt.Fprintln(os.Stderr, "Cannot write to output file")
+		os.Exit(1)
+	}
+
+	handler := lab2.ComputeHandler{R: readStream, W: writeStream}
+	err := handler.Compute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
